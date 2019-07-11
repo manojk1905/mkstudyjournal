588d4e03406c25b3da9d71370a36ef319a64b9c5
manojk1905 <31339962+manojk1905@users.noreply.github.com>
2019-07-11 15:07:39 +0530

    Spring boot docker app

---
+++ spring-boot-docker-app/src/main/java/com/mk/SpringBootConfig.java
@@ -0,0 +1,11 @@
+package com.mk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootConfig {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootConfig.class, args);
+    }
+}
